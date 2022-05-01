0b60d21f65e92fada02db4c7cd361e1b6a4ffbfa
Erfan Sabouri <erfaansabouri@gmail.com>
2022-05-02 02:56:20 +0430

    Initial.

---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <header class="py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-lg-4 pt-1">
+                <img src="{{ asset('assets/image/logo.svg') }}" alt="Logo Golshateb">
+            </div>
+            <div class="d-none d-lg-block col-lg-4 text-center">
+                <div class="input-group">
+                    <div class="input-group-append">
+                        <button class="btn golshateb-bg-grey rounded-right" type="button"><img src="{{ asset('assets/svg/golshateb/search.svg') }}" alt="Golshateb"></button>
+                    </div>
+                    <input type="text" class="form-control golshateb-bg-grey rounded-left" placeholder="جستجو در گلشا">
+                </div>
+
+            </div>
+            <div class="col-lg-4 col-sm-6 d-flex justify-content-end align-items-center">
+                <a class="btn btn-sm btn-outline-secondary ml-3" href="#">ورود/عضویت</a>
+                <button class="btn btn-sm btn-light-success">
+                    <img src="{{ asset('assets/svg/golshateb/shopping-bag.svg') }}" alt="Golshateb">
+                </button>
+            </div>
+        </div>
+    </header>
+</div>
