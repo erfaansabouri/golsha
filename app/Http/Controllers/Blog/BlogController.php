b35a92fc810be07a3dff3381a41d93468f9c5bcf
Erfan Sabouri <erfaansabouri@gmail.com>
2022-06-29 13:22:09 +0430

    Fix

--- app/Http/Controllers/Blog/BlogController.php
+++ app/Http/Controllers/Blog/BlogController.php
@@ -13,39 +13,39 @@ class BlogController extends Controller
 	protected $pageInfo = [
 		'title' => 'وبلاگ گلشاتب',
 	];
-	
+
 	private function footerTags()
 	{
 		$tags = [];
 		$blogPosts = BlogPost::query()->whereNotNull('tags')->get();
 		foreach ($blogPosts as $blogPost)
 		{
-			$exploded = explode(',',$blogPost->tags);
+			$exploded = explode('،',$blogPost->tags);
 			foreach ($exploded as $tag)
 			{
 				$tags[] = $tag;
 			}
 		}
-		
+
 		return collect($tags)->take(20)->all();
 	}
-	
+
 	private function ads()
 	{
 		$ads = Setting::query()->where('type', Setting::TYPES['image'])->take(3)->get();
 		return $ads;
 	}
-	
+
 	private function description()
 	{
 		return 'test';
 	}
-	
+
     public function index(Request $request)
 	{
 		$ads = $this->ads();
 		$blogPosts = BlogPost::query()->orderByDesc('id');
-		
+
 		if(!empty($request->tag))
 		{
 			$blogPosts = $blogPosts->where('tags', 'like', '%'. $request->tag .'%');
@@ -63,13 +63,13 @@ class BlogController extends Controller
 				->pluck('blog_post_id');
 			$blogPosts = $blogPosts->whereIn('id', $ids);
 		}
-		
+
 		$blogPosts = $blogPosts->paginate(10);
 		$tags = $this->footerTags();
 		$footerDescription = $this->description();
 		return view('blog-pages.index', compact('blogPosts', 'ads', 'tags', 'footerDescription'))->with('pageInfo', $this->pageInfo);
 	}
-	
+
 	public function show($id)
 	{
 		$ads = $this->ads();
