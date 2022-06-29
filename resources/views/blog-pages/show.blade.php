0f4bccb4fffea5f00f95e3b98d14537c063cb129
Erfan Sabouri <erfaansabouri@gmail.com>
2022-06-29 14:17:01 +0430

    Fix

--- resources/views/blog-pages/show.blade.php
+++ resources/views/blog-pages/show.blade.php
@@ -60,7 +60,7 @@
                                         </div>
                                     </div>
                                     <div class="blgSnglPstTxt" style="font-size: 16px; margin: 0">
-                                        {!! str_replace( "</p><p>","</p><br><p>",$blogPost->body ); !!}
+                                        {!! str_replace( "<p></p>","<p><br></p>",$blogPost->body ); !!}
                                     </div>
                                     <div class="blgSnglTags">
                                         <div class="blgSnglTgHed">
